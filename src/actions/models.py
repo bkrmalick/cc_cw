dce095f84c3ec6b23c7496b740343006c43047c6
bkrmalick_CCVM <bkrmalick@hotmail.com>
2020-03-19 00:21:26 +0000

    redesigning db schema, combining serializers for auction and item

--- src/actions/models.py
+++ src/actions/models.py
@@ -5,17 +5,23 @@ CONDITION_CHOICES = (
     ("poor","POOR"),
     ("good","GOOD"),
     ("new","NEW"),
-
 )
+
 class Item(models.Model):
     ItemCode=models.CharField(max_length=10, primary_key=True)
     Title=models.CharField(max_length=50)
-    Condition=models.CharField(max_length=5, choices=CONDITION_CHOICES,default='good')
     Description=models.CharField(max_length=200)
+    FirstRegDate=models.DateTimeField()
+    
+    def __str__(self):
+        return self.ItemCode+" ("+self.Title+")"
     
 class Auction(models.Model):
-    ItemCode=models.ForeignKey(Item, on_delete=models.PROTECT) #do not allow deletion of item before auction
+    #AuctionID - automatic
+    Item=models.ForeignKey(Item, on_delete=models.PROTECT) #do not allow deletion of item before auction
+    ItemCondition=models.CharField(max_length=5, choices=CONDITION_CHOICES,default='good')
     CreatedByUsername=models.ForeignKey(User, on_delete=models.PROTECT) 
+    Brief=models.TextField()
     StartDate=models.DateTimeField()
     EndDate=models.DateTimeField()
     MinimumPrice=models.FloatField()
--- src/actions/serializers.py
+++ src/actions/serializers.py
@@ -1,15 +1,29 @@
 from rest_framework import serializers
+from rest_framework.validators import UniqueValidator
 from .models import Item, Auction
-
+import datetime
 
 class ItemSerializer(serializers.ModelSerializer):
+    #validation to ensure code input is unique
+    ItemCode =serializers.CharField(max_length=10,validators=[UniqueValidator(queryset=Item.objects.all(), message="ItemCode already exists")])
+    
     class Meta:
         model = Item
-        fields = ('ItemCode','Title','Condition','Description')
-        
-        
+        fields = ('ItemCode','Title','Description')
+    
+    def create(self, validated_data):
+        return Item.objects.create(**validated_data, FirstRegDate=datetime.datetime.now())
+
 
 class AuctionSerializer(serializers.ModelSerializer):
+    Item=ItemSerializer()
+
     class Meta:
         model = Auction
-        fields = ('ItemCode','CreatedByUsername','StartDate','EndDate','MinimumPrice')
+        fields = ('Item','ItemCondition','CreatedByUsername','Brief','StartDate','EndDate','MinimumPrice')
+        
+    ##TO-DO    
+    ##def create(self, validated_data):
+    ##    return Auction.objects.create(**validated_data, CreatedByUsername=)
+  
+  
