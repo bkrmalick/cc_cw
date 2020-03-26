433e3f51fccc565bd7a22670653e31434be9a1e7
bkrmalick <bkrmalick@hotmail.com>
2020-03-26 01:49:23 +0000

    auto detect username

--- testcases.py
+++ testcases.py
@@ -1,4 +1,4 @@
-import json, requests
+import json, requests, datetime
 ###########################GLOBAL VARIABLES###########################
 
 reg_url="http://193.61.36.131:8000/authentication/register/"
@@ -22,9 +22,9 @@ if(create_user_flag==1):
     mary_response=requests.post(reg_url,data=mary).json()
     #olga_response=requests.post(revoke_url, data={"token":"iGeEjKoNUHhOIu1J4DF39yejIljE9G"}).json()
 
-    print("\nOlga Response:",olga_response)
-    print("\nNick Response",nick_response)
-    print("\nMary Response",mary_response)
+    print("\nOlga Response: ",olga_response)
+    print("\nNick Response: ",nick_response)
+    print("\nMary Response: ",mary_response)
 
 print("############-TEST CASE 2-############")
 
@@ -34,23 +34,40 @@ mary_response=requests.post(token_url,data=mary).json()
 
 
 print("\nOlga Response:",olga_response)
-print("\nNick Response",nick_response)
-print("\nMary Response",mary_response)
+print("\nNick Response:",nick_response)
+print("\nMary Response:",mary_response)
 
 olga_token=str(olga_response['access_token'])
-olga_token=str(nick_response['access_token'])
-olga_token=str(mary_response['access_token'])
+nick_token=str(nick_response['access_token'])
+mary_token=str(mary_response['access_token'])
 
 
-print("############-TEST CASE 3-############")
+print("\n############-TEST CASE 3-############")
 
-#olga_response=requests.get(auctions_url).json()
-#print(olga_response)
+olga_response=requests.get(auctions_url, headers= {'Authorization':'Bearer '+""}).json()
+print("Olga request without token:\n "+str(olga_response))
 
-olga_response=requests.get(auctions_url, headers= {'Authorization':'Bearer '+olga_token}).json()
+olga_response=requests.get(auctions_url, headers= {'Authorization':'Bearer '+olga_token})
 
-print(olga_response)
+print("Olga request with token:\n "+json.dumps(olga_response.json(), indent=2))
+
+print("\n############-TEST CASE 4-############")
+
+olga_auction={
+    "Item": {
+        "Title": "Olga's Shirt",
+        "ItemCondition": "good",
+        "Description": "D&C shirt, hasn't been worn in years"
+    },
+    #"CreatedByUsername": 1,
+    "Brief": "Serious buyers only",
+    "StartDate": str(datetime.datetime.now()),
+    "EndDate": str(datetime.datetime(2020, 3, 27, 23, 35, 14, 496045)),
+    "MinimumPrice": 20
+}
 
 
+olga_response=requests.post(auctions_url,json=olga_auction, headers={'Content-type': 'application/json','Authorization':'Bearer '+olga_token})
 
-
+print(olga_response.text)
+print(olga_response.request.body)
