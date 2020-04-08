dad10ac0110874f5f5c2fc2015897c796c33c0ca
bkrmalick <bkrmalick@hotmail.com>
2020-04-08 02:14:06 +0100

    completed 13 test cases

--- testcases.py
+++ testcases.py
@@ -1,4 +1,4 @@
-import json, requests, datetime
+import json, requests, datetime, time
 ###########################GLOBAL VARIABLES###########################
 
 reg_url="http://193.61.36.131:8000/authentication/register/"
@@ -6,25 +6,34 @@ revoke_url="http://193.61.36.131:8000/authentication/token/revoke/"
 token_url="http://193.61.36.131:8000/authentication/token/"
 
 auctions_url="http://193.61.36.131:8000/actions/auction/"
+bid_url="http://193.61.36.131:8000/actions/bid/"
 
 olga={"username": "olga", "password": "olga123"}
 nick={"username": "nick", "password": "nick123"}
 mary={"username": "mary", "password": "mary123"}
 admin={"username": "bkr", "password": "bkr"}
+
+MARY_AUCTION_DURATION_IN_SECONDS=5
 ###########################HELPER FUNCTIONS############################
 
 def prettyResponse(resp):
-    return (json.dumps(resp.json(), indent=2))
+    return str(resp)+"\n"+ str(json.dumps(resp.json(), indent=2))
 
 def resetApplication(): #deletes all auctions, access tokens, users
+    print("\nTrying to reset API data for demo...")
     #get new token for admin user
     token=requests.post(token_url,data=admin).json()["access_token"]
 
     #delete all auctions, items, and bids (only works with admin token) 
-    requests.delete(auctions_url,headers= {'Authorization':'Bearer '+token})   
- 
-    #delete all users and their tokens
-    #todo
+    resp=requests.delete(auctions_url,headers= {'Authorization':'Bearer '+token})
+    
+    print(resp)
+    print(resp.reason+"\n")
+
+    if(resp.ok):
+        print("Successfully deleted previous data!\n")
+    else:
+        print("Error, check details above \n")
 
 ###########################PRE-TEST ACTIONS############################
 
@@ -97,8 +106,8 @@ nick_auction={
         "Description": "left joystick doesn't work, salvageable for other parts"
     },
     "Brief": "Buyer must be willing to pick up item from Croydon",
-    "StartDate": str(datetime.datetime.now()),                          #todays date
-    "EndDate": str(datetime.datetime.now()+datetime.timedelta(days=1)), #todays date +1 day
+    "StartDate": str(datetime.datetime.now()+datetime.timedelta(days=1)),#todays date +1 days
+    "EndDate": str(datetime.datetime.now()+datetime.timedelta(days=2)),  #todays date +2 day
     "MinimumPrice": 30
 }
 
@@ -118,8 +127,8 @@ mary_auction={
         "Description": "Cara Glass Touch Lamps, Set of 2 - connects to WiFi and controllable with phone"
     },
     "Brief": "",
-    "StartDate": str(datetime.datetime.now()),                          #todays date
-    "EndDate": str(datetime.datetime.now()+datetime.timedelta(days=1)), #todays date +1 day
+    "StartDate": str(datetime.datetime.now()),                             #todays date
+    "EndDate": str(datetime.datetime.now()+datetime.timedelta(seconds=MARY_AUCTION_DURATION_IN_SECONDS)), #will end seconds after starting
     "MinimumPrice": 40
 }
 
@@ -129,7 +138,7 @@ mary_response=requests.post(auctions_url,json=mary_auction, headers={'Content-ty
 print("\nResponse of Mary adding new auction: ")
 print(prettyResponse(mary_response))
 
-print("\n############-TEST CASE 7-############")
+print("\n############-TEST CASE 7 & 8-############")
 
 olga_response=requests.get(auctions_url, headers= {'Authorization':'Bearer '+olga_token})
 
@@ -140,3 +149,75 @@ nick_response=requests.get(auctions_url, headers= {'Authorization':'Bearer '+nic
 
 print("\nNICK BROWSING AUCTIONS: ")
 print(prettyResponse(nick_response))
+
+print("\n############-TEST CASE 9-############")
+
+for i in nick_response.json(): #can be either one of three peoples response as they all will contain marys auction
+	if(i["CreatedByUsername"]=="mary"):
+		marys_auction=i
+
+mary_bid={
+    "Auction": marys_auction["id"],
+    "BidPrice": 29.87
+}
+
+mary_response=requests.post(bid_url,json=mary_bid, headers={'Content-type': 'application/json','Authorization':'Bearer '+mary_token})
+
+print("\nResponse of Mary bidding on her own auction: "+prettyResponse(mary_response))
+
+
+print("\n############-TEST CASE 10-############")
+
+#Olga's bid on marys item
+olga_bid={
+    "Auction": marys_auction["id"],
+    "BidPrice": 45
+}
+
+olga_response=requests.post(bid_url,json=olga_bid, headers={'Content-type': 'application/json','Authorization':'Bearer '+olga_token})
+
+print("\nResponse of Olga bidding on Mary's auction: "+prettyResponse(olga_response))
+
+
+#Nick's bid on marys item
+nick_bid={
+    "Auction": marys_auction["id"],
+    "BidPrice": 60
+}
+
+nick_response=requests.post(bid_url,json=nick_bid, headers={'Content-type': 'application/json','Authorization':'Bearer '+nick_token})
+
+print("\nResponse of Nick bidding on Mary's auction: "+prettyResponse(nick_response))
+
+print("\n############-TEST CASE 11-############")
+
+print("Waiting for Mary's auction to finish...")
+
+for i in range(MARY_AUCTION_DURATION_IN_SECONDS):
+    time.sleep(i+1)
+    print("Time Passed: "+str(i+1)+" seconds")
+
+nick_response=requests.get(auctions_url+str(marys_auction["id"])+"/", headers= {'Authorization':'Bearer '+nick_token})
+print("\nResponse of Nick GET-ting Mary's auction to see if he's won: ")
+print(prettyResponse(nick_response))
+
+print("\n############-TEST CASE 12-############")
+
+olga_response=requests.get(auctions_url, headers= {'Authorization':'Bearer '+olga_token})
+
+print("\nOLGA BROWSING all the items sold: ")
+print(olga_response)
+for i in olga_response.json(): 
+	if(i["Status"]=="COMPLETED"):
+		print(json.dumps(i["Item"], indent=2))
+
+print("\n############-TEST CASE 13-############")
+
+mary_response=requests.get(bid_url, headers= {'Authorization':'Bearer '+mary_token})
+
+print("\nMary querying for all bids on her auction: ")
+print(mary_response)
+for i in mary_response.json(): 
+	if(i["Auction"]==marys_auction["id"]):
+		print(json.dumps(i, indent=2))
+
